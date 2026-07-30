279263c7cb3f56c05c5458755fcd28e939c66fb9
silvarc141 <silvarc141@users.noreply.github.com>
2026-07-30 23:18:42 +0200

    termux.sh

--- termux.sh
+++ termux.sh
@@ -1,19 +1,20 @@
-# WARNING: OUTDATED
-
-termux-change-repo
-pkg upgrade -y
-pkg install nushell git git-crypt ripgrep 
-
-# Optional setup:
-# chsh nu
+# Optional before: 
+# termux-change-repo
+# pkg upgrade -y
 
 # Run with something like this:
 # . $(curl -s https://raw.githubusercontent.com/silvarc141/cross-config/main/termux.sh)
 
+# Optional after: 
+# chsh nu
 
-# nu -c '
-# let lib = http get https://raw.githubusercontent.com/silvarc141/notes-nu/main/lib.nu
-# let dir = $nu.user-autoload-dirs | first
-# mkdir $dir
-# $lib o> ($dir | path join "notes.nu")
-# '
+pkg install nushell git git-crypt ripgrep 
+
+nu -c '
+let dir = $nu.user-autoload-dirs | first
+mkdir $dir
+cd $dir
+git clone https://github.com/silvarc141/cross-config.git
+cp -rf ./cross-config/nu/* ./
+rm -rf ./cross-config
+'
